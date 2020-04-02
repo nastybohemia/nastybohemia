6ffa7c69740fbf2be8e9799e180262db4dd8c78e
Nasty Bohemia <sophie@post-production.team>
2020-04-02 15:23:41 -0400

    header

---
+++ header.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>header</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="mylead-verification" content="ff89b1b9530c12e46c7b2ff6c924c016">
+    <!-- ************************* JQuery *************************-->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+    crossorigin="anonymous"></script>
+    <!-- ************************* Bootstrap *************************-->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <style>
+            @import "https://use.typekit.net/swk3xjq.css";
+html, body, #app {
+  height: 100%;
+}
+
+body {
+  background-color: #456;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='10' height='10' viewBox='0 0 4 4'%3E%3Cpath fill='%23fff' fill-opacity='0.1' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E");
+  margin: 0;
+  overflow: hidden;
+  text-align: center;
+}
+        .navbar-brand{
+            background-image:url('assets/img/bandito-profile-3.jpg');
+            background-size: 50px;
+            width:50px;
+        }
+    </style>
+    </head>
+    <body>
+        <header>
+        <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display <img scr="assets/img/instagram.png" alt="Instagram">-->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="https://www.instagram.com/nastybohemia/" target="_blank"></a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li><a href="/">COVID-19 Oracle<span class="sr-only">(current)</span></a></li>
+        <li><a href="cases.php">COVID-19 Latest Data</a></li>
+        <li><a href="#">Draw Art (coming soon)</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Tell me more<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">GitHub (coming soon)</a></li>
+            <li><a href="#">Blog (coming soon)</a></li>
+            <li><a href="#">Twitter (coming soon)</a></li>
+            <li><a href="https://www.instagram.com/nastybohemia/" target="_blank">Instagram</a></li>
+          </ul>
+        </li>
+      </ul>
+      
+    </div><!-- /.navbar-collapse<ul class="nav navbar-nav navbar-right">
+          <form class="navbar-form navbar-left">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="E-mail">
+        </div>
+        <button type="submit" class="btn btn-default">Send me updates!</button>
+      </form>
+      </ul> -->
+  </div><!-- /.container-fluid -->
+</nav>
+</header>
+    </body>
+</html>
